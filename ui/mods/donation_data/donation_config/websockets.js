71b905acfd0e6fe52f5673ecf53e38383ff2d1cd
Justin Love <git@JustinLove.name>
2017-11-02 14:39:46 -0500

    add websocket client

---
+++ ui/mods/donation_data/donation_config/websockets.js
@@ -0,0 +1,42 @@
+define([
+  'donation_data/donation_config/parse',
+  'donation_data/donation_config/live',
+  'donation_data/config',
+], function(parse, poll, config) {
+  var local = "ws://localhost:5000/donations"
+  var donations = local
+
+  var query = ""
+  if (config.current_match()) {
+    query = "?untagged=true&game=" + config.current_match()
+  }
+
+  var subscribe = function(callback) {
+    var ws = new WebSocket(donations + query)
+
+    ws.onerror = function() {
+      console.log('websocket error', arguments)
+    }
+
+    ws.onclose = function() {
+      console.log('websocket opened')
+    }
+
+    ws.onclose = function() {
+      console.log('websocket closed')
+    }
+
+    ws.onmessage = function(event) {
+      //console.log('message', event)
+      var message = JSON.parse(event.data)
+      callback(parse.process(message))
+    }
+  }
+
+  return {
+    donations: donations,
+    update: poll.update,
+    subscribe: subscribe,
+    process: parse.process,
+  }
+})
