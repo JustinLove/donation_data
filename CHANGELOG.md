c5aca88da540d006e155dedbf7e4845637c32d15
Justin Love <git@JustinLove.name>
2017-11-14 11:39:42 -0600

    extract discount level from our feed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,12 @@
 # Donation Data Changelog
 
+## 0.2.0
+
+- Extract discount level from our feed
+
 ## 0.1.0
 
-Websocket client
+- Websocket client
 
 ## 0.0.0
 
--- ui/mods/donation_data/donation_config/parse.js
+++ ui/mods/donation_data/donation_config/parse.js
@@ -6,6 +6,7 @@ define([], function() {
       var comment = donation.comment || ''
       var donor_name = donation.donor_name
       var donor_image = donation.donor_image
+      var discount_level = donation.discount_level
 
       return {
         amount: amount,
@@ -13,6 +14,7 @@ define([], function() {
         donor_name: donor_name,
         donor_image: donor_image,
         id: id,
+        discount_level: discount_level,
       }
     }).reverse()
   }
